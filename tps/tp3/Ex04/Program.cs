4c39747d1eb088bbdd78d3a1dbd9dd5b07d2a9fd
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 09:28:10 -0300

    ex04

---
+++ tps/tp3/Ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ingresso ingresso = new Ingresso("Show da xuxa", 4, 2500);
+        
+        ingresso.AlterarPreco(3000);
+        ingresso.AlterarQuantidade(10);
+        ingresso.ExibirInformacoes();
+    }
+}
