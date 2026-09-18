934af763c0241ebd823619f23f06b17643e12cd3
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 11:10:24 -0300

    add construdor

--- tps/tp3/Ex11/Circulo.cs
+++ tps/tp3/Ex11/Circulo.cs
@@ -3,7 +3,12 @@ namespace Ex11;
 public class Circulo
 {
     
-    public double Raio { get; set; }
+    public double Raio { get; private set; }
+    
+    public Circulo(double raio)
+    {
+        Raio = raio;
+    }
     
     public double CalcularArea() {
         return Math.PI * Raio * Raio;
--- tps/tp3/Ex11/Esfera.cs
+++ tps/tp3/Ex11/Esfera.cs
@@ -2,7 +2,12 @@ namespace Ex11;
 
 public class Esfera
 {
-    public double Raio { get; set; }
+    public double Raio { get; private set; }
+    
+    public Esfera(double raio)
+    {
+        Raio = raio;
+    }
     
     public double CalcularVolume() {
         return 4.0 / 3.0 * Math.PI * Raio * Raio * Raio;
