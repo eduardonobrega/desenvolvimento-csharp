e8064a89ddbdde33489bcadf7d0bc72355306514
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 10:28:15 -0300

    ex07

---
+++ tps/tp3/Ex07/Matricula.cs
@@ -0,0 +1,33 @@
+namespace Ex07;
+
+public class Matricula
+{
+    public string NomeDoAluno { get; set; }
+    public string Curso { get; set; }
+    public int NumeroMatricula { get; private set; }
+    public string Situacao { get; private set; }  // Ativa", "Trancada", "Concluída"
+    public string DataInicial { get; private set; } // data em que a matrícula foi iniciada
+
+
+    public Matricula(string nomeDoAluno, string curso, int numeroMatricula)
+    {
+        
+        if (string.IsNullOrWhiteSpace(nomeDoAluno))
+            throw new ArgumentException("O nome do aluno não pode ser vazio.");
+
+        if (string.IsNullOrWhiteSpace(curso))
+            throw new ArgumentException("O curso não pode ser vazio.");
+
+        if (numeroMatricula < 1)
+        {
+            throw new ArgumentException("Número da matricula inválido.");
+        }
+        
+        NomeDoAluno = nomeDoAluno;
+        Curso = curso;
+        NumeroMatricula = numeroMatricula;
+        
+        Situacao = "Ativa";
+        DataInicial = DateTime.Now.ToString("dd/MM/yyyy");
+    }
+}
---
+++ tps/tp3/Ex07/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Ex07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matricula matricula = new Matricula("Eduardo", "ADS", 123);
+    }
+}
