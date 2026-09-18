f72525b08857c314f25b5d6f9ec45fd2f8062424
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 10:30:34 -0300

    ex09

---
+++ tps/tp3/Ex09/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex09;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Matricula matricula = new Matricula("Eduardo", "ADS", 123);
+        matricula.ExibirInformacoes();
+        matricula.Trancar();
+        matricula.ExibirInformacoes();
+        matricula.Reativar();
+        matricula.ExibirInformacoes();
+    }
+}
