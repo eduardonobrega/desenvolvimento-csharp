1f379c9603142bcd7243fdc096ea58be0c2e6358
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 09:51:58 -0300

    removendo trechos mais avançados

--- tps/tp3/Ex04/Program.cs
+++ tps/tp3/Ex04/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Ingresso ingresso = new Ingresso("Show da xuxa", 4, 2500);
+        Ingresso ingresso = new Ingresso();
+        ingresso.nomeDoShow = "Show da xuxa";
+        ingresso.preco = 2500;
+        ingresso.quantidadeDisponivel = 4;
+        
         
         ingresso.AlterarPreco(3000);
         ingresso.AlterarQuantidade(10);
