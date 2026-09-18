68e29c01f1238aab063503c9546371d8c1695fb6
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-18 09:55:37 -0300

    removendo trechos avançados

--- tps/tp3/Ex02/Ingresso.cs
+++ tps/tp3/Ex02/Ingresso.cs
@@ -2,7 +2,7 @@ namespace Ex02;
 
 public class Ingresso
 {
-    public string NomeDoShow { get; set; } // para identificar o show
-    public double Preco { get; set; }     // para saber quanto cobrar do cliente
-    public int QuantidadeDisponivel { get; set; } // para saber a quantidade disponivel e se ainda é possivel vender caso seja > 0
+    public string nomeDoShow; // para identificar o show
+    public double preco;     // para saber quanto cobrar do cliente
+    public int quantidadeDisponivel; // para saber a quantidade disponivel e se ainda é possivel vender caso seja > 0
 }
--- tps/tp3/Ex02/Program.cs
+++ tps/tp3/Ex02/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Ingresso ingresso = new Ingresso();
+        ingresso.nomeDoShow = "Show da xuxa";
+        ingresso.preco = 2500;
+        ingresso.quantidadeDisponivel = 4;
     }
 }
