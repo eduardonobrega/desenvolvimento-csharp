bddd532709a92db5f52c771f27d7dee94fb9da5b
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-22 19:04:53 -0300

    ex03

---
+++ at/Ex03/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Ex03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = lerInteiro("Insira um número: ");
+        int outroNumero = lerInteiro("Insira outro número: ");
+
+        Console.WriteLine("=== Escolha uma opção ===");
+        Console.WriteLine("1. Soma");
+        Console.WriteLine("2. Subtração");
+        Console.WriteLine("3. Multiplicação");
+        Console.WriteLine("4. Divisão");
+        Console.Write(": ");
+        int op =  lerInteiro("");
+
+        if (op < 1 || op > 4)
+        {
+            Console.WriteLine("Opção inválida");
+        }
+        else
+        {
+            double? resultado = op switch
+            {
+                1 => numero + outroNumero,
+                2 => numero - outroNumero,
+                3 => numero * outroNumero,
+                4 => outroNumero != 0 ? (double) numero / outroNumero : null
+            };
+
+            if (resultado == null)
+            {
+                Console.WriteLine("Não é possivel dividir por 0!");
+            }
+
+            else
+            {
+                Console.WriteLine("Resultado: " + resultado);
+            }
+        }
+    }
+    
+    // Função para ler e validar a entrada do usuário
+    static int lerInteiro(string msg)
+    {
+        int numero;
+
+        Console.Write(msg);
+        while (!int.TryParse(Console.ReadLine(), out numero))
+        {
+            Console.WriteLine("Erro: Isso não é um número válido!");
+            Console.Write("Tente novamente :");
+        }
+
+        return numero;
+    }
+}
