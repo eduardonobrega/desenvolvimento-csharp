77ca2e04df7ac297317f781c2e9d30059f7a8590
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-22 18:25:40 -0300

    ex01

---
+++ at/Ex01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, meu nome é Eduardo!");
+        Console.WriteLine("Nasci em 08 de julho de 1999 e estou aprendendo c#!");
+    }
+}
