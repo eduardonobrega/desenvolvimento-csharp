df2ff64d2884c34af86c2214b63660f5164b2a27
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2026-09-22 19:24:09 -0300

    ex06

---
+++ at/Ex06/Aluno.cs
@@ -0,0 +1,30 @@
+namespace Ex05;
+
+public class Aluno
+{
+    private string Nome;
+    private string Matricula;
+    private string Curso;
+    private double Media;
+
+    public Aluno(string nome, string matricula, string curso, double media)
+    {
+        Nome = nome;
+        Matricula = matricula;
+        Curso = curso;
+        Media = media;
+    }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("Matricula: " + Matricula);
+        Console.WriteLine("Curso: " + Curso);
+        Console.WriteLine("Media: " + Media);
+    }
+
+    public string VerificarAprovacao()
+    {
+        return Media >= 7 ? "Aprovado" : "Reprovado";
+    }
+}
---
+++ at/Ex06/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ex05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno a1 = new Aluno("Eduardo", "ads123", "ADS", 8);
+        
+        a1.ExibirDados();
+        Console.WriteLine(a1.VerificarAprovacao());
+    }
+}
